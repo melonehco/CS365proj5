c140d7203a2fb82bd8561db1d7d3979c3e992a91
melonehco <mzmao@colby.edu>
2019-04-19 13:34:31 -0400

    finished up to task 2B

---
+++ analyzeNetwork.py
@@ -0,0 +1,72 @@
+"""
+analyzeNetwork.py
+loads a network from a file to do digit recognition using the MNIST data base
+
+Zena Abulhab and Melody Mao
+CS365 Spring 2019
+Project 5
+"""
+
+import sys
+import keras
+import matplotlib
+matplotlib.use('Agg') # for saving plot to file
+from matplotlib import pyplot
+import numpy
+
+def main( argv ):
+	# load model from file
+	model = keras.models.load_model( "./mnistModel.h5" )
+	weights = model.layers[0].get_weights()[0] # index 0 for weights, 1 for bias
+	
+	print("Loaded in model")
+	print("weights dims:", weights.shape)
+	
+	for i in range(weights.shape[3]):
+		print("filter", i)
+		for x in range(weights.shape[0]):
+			line = ""
+			for y in range(weights.shape[1]):
+				line += " " + str(weights[x][y][0][i])
+			print(line)
+		print()
+	
+	#fig, axs = pyplot.subplots( 4, 8, figsize=(5, 5) )
+	fig = pyplot.figure()
+	
+	#----- adapted from: https://stackoverflow.com/questions/49775515/visualise-filters-in-keras-cnn
+	# normalize these filters first, otherwise they won't be in a suitable range for plotting:
+	maxVal = weights.max( )
+	minVal = weights.min( )
+	absMax = max( abs( minVal ), abs( maxVal ) )
+	
+	normWeights = (weights / absMax) * 255
+	
+	for filterNum in range( 32 ):
+		filter = normWeights[ :, :, 0, filterNum ]
+		
+		# a trick to see negatives as blue and positives as red
+		imageRed = numpy.array( filter )
+		imageBlue = numpy.array( filter )
+		imageRed[ imageRed < 0 ] = 0
+		imageBlue[ imageBlue > 0 ] = 0
+		
+		# print( imageRed )
+		# print( imageBlue )
+		
+		redBlueGrid = numpy.zeros( (filter.shape[ 0 ], filter.shape[ 1 ], 3) ) # 3x3, 3 color channels
+		redBlueGrid[ :, :, 0 ] = imageRed
+		redBlueGrid[ :, :, 2 ] = -imageBlue
+		
+		# plot image here
+		pyplot.subplot(8, 4, filterNum+1)
+		pyplot.imshow( redBlueGrid )
+	#-----------------------------------------------------------------------------------------------
+	
+	input()
+	pyplot.savefig( "testing.pdf" )
+	
+	#axs[ 1, 1 ].hist2d( data[ 0 ], data[ 1 ] )
+
+if __name__ == "__main__":
+	main( sys.argv )
--- loadNetwork.py
+++ loadNetwork.py
@@ -7,18 +7,57 @@ CS365 Spring 2019
 Project 5
 """
 
+import sys
 import numpy
 import keras
 from buildNetwork import loadData
+from pathlib import Path
+import cv2
 
-def main():
+def main(argv):
 	
 	# input image dimensions
 	img_rows, img_cols = 28, 28
 	
-	x_train, y_train, x_test, y_test, input_shape = loadData()
-	x_test = x_test[0:10:1]
-	y_test = y_test[0:10:1]
+	#x_test = None
+	#y_test = None
+	if len(argv) > 1:  # if a data source folder was provided
+		path = Path( argv[1] )
+		
+		if not path.is_dir():
+			print("Error:", argv[1], "is not a directory")
+			exit()
+		
+		imgPaths = list( path.glob( '**/*.jpg' ) )
+		imgNames = []
+		for p in imgPaths:
+			imgNames.append( str(p) )
+		imgNames.sort()
+		print(imgNames)
+		
+		images = []
+		i = 0
+		for n in imgNames:
+			img = cv2.imread( n, cv2.IMREAD_COLOR )
+			img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
+			cv2.imshow("image {:d}".format(i), img)
+			img = cv2.resize( img, (img_rows, img_cols) )
+			img = img[:, :, numpy.newaxis]
+			images.append( img )
+			i += 1
+		
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+		
+		x_test = numpy.asarray( images )
+		print("xtest shape", x_test.shape)
+		y_test = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		#y_test = [2, 6, 8, 0, 1, 4, 7, 3, 5, 9]
+		input()
+	else:
+		x_train, y_train, x_test, y_test, input_shape = loadData()
+		x_test = x_test[0:10:1]
+		y_test = y_test[0:10:1]
 	
 	# load model from file
 	model = keras.models.load_model("./mnistModel.h5")
@@ -37,6 +76,11 @@ def main():
 		print("  max index:", maxIdx)
 		
 		print("  true label:", y_test[i])
+	
+	predClasses = model.predict_classes( x_test )
+	
+	for i in range( len( predClasses ) ):
+		print( "predicted:", predClasses[i], "true:", y_test[i] )
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
